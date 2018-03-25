d51c4c9cf3e1f7238c119aa10c09e142b5dc5979
Nick <Nick@Nicks-iMac-2.home>
2018-03-25 13:24:09 -0400

    added lesson 7 homework.js

--- Lesson7-JS-IV/homework/README.md
+++ Lesson7-JS-IV/homework/README.md
@@ -5,10 +5,19 @@
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
 	* Objects
+	Objects are the underlying structure of the javascript framework.  They allow for the creation of custom methods for the manipulation of properties, they store data and represent it in relation to keys instead of order.
+
 	* Properties
+	When defining an object, the attributes are titled and assigned values using this naming convention. Built in properties are characteristics of all object types.
+
 	* Methods
+	Operate predefined actions on data in arrays and strings or custom rules when created in Objects.
+
 	* for in loop
+	A for loop specifically for objects, allows for the operation of methods through properties in an object by using keys instead of sequential order. 
+
 	* Dot notation vs bracket notation
+	Methods are called on to act on properties using both dot and bracket notation.  Dot notation is the best practice when updating object keys, bracket notation allows properties to be reassigned without knowing the what the variable is set to. 
 
 2. From the top level of your `Precourse` folder, run `npm test JSIV.test.js` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -2,69 +2,122 @@
 
 function makeCat(name, age) {
   // create a new object with a name property with the value set to the name argument
+  const catName = {
+    addName: name,
+    addAge: age,
+    Meow: function() {
+      console.log('Meow!');
+  },
+  }
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  return catName{};
+
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
+  const keyName = {
+    addProperty.property(),
+  }
+  return object
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
+  const methodName: {
+    getMethod = method;
+  }
   // invoke this method
   // nothing needs to be returned
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
+  const mysteryProduct = {
+    mysteryNumber = mysteryNumberObject * 5;
+  }
+return mysterProduct;
   // multiply the mysteryNumber property by 5 and return the product
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
+  delete.arguments[property];
   // return the object
+  return deleteProperty();
+
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
+  const newObj = {
+    name: newUser[name];
+    email: newUser[email];
+    password: newUser[password];
+  }
   // return the new object
+  return newObj[];
 }
 
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
+  const containsEmail = false;
+    for (let key in user){
+    if ( key === "email"){
+      hasEmail.containsEmail[true];
+    } 
+return containsEmail[];
   // otherwise return false
 }
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
+  const checkProperty = false;
+    for (let object in hasProperty){
+    if ( object === property){
+      hasProperty.checkProperty[true];
+    } 
   // property is a string
   // otherwise return false
+  return checkProperty[];
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
+  const checkPassword = false;
+  if (user.password() === )
+
+
+  }
   // return true if they match
   // otherwise return false
 }
 
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
+  const changePW = {
+    user.password = newPassword;
+  }
   // return the object
 }
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
+  user.push(newFriend);
   // add newFriend to the end of the friends array
   // return the user object
+  return user[];
 }
 
 function setUsersToPremium(users) {
   // users is an array of user objects.
   // each user object has the property 'isPremium'
+  for (let key in users)
+  users.isPremium[true];
   // set each user's isPremium property to true
   // return the users array
 }
